73d76f50c912fab2ce23f2ad648d875ad2d42d41
Cole Flournoy <flournoy.cole@gmail.com>
2021-05-09 18:08:19 -0400

    Refactor to Deck class

--- flashcard-frontend/src/deck.js
+++ flashcard-frontend/src/deck.js
@@ -1,64 +1,81 @@
-function fetchDecks(){
-  fetch("http://localhost:3000/decks")
-  .then(resp => resp.json())
-  .then(appendDecks)
-}
+class Deck {
+  constructor(deck){
+    this.id = deck.id
+    this.name = deck.name
+    this.group = deck.group
+    this.cards = deck.cards
+  }
 
-function appendDecks(decks){
-  const detailDiv = document.getElementById('detailContainer')
-  detailDiv.innerText = ''
-  const deckDiv = document.getElementById('indexContainer')
-  deckDiv.innerText = "All My Decks!"
-  for (let deck of decks){
-    const li = document.createElement('li')
-    li.innerText = `${deck.name} (${deck.group.name})`
-    deckDiv.append(li)
+  static fetchDecks(){
+    fetch("http://localhost:3000/decks")
+    .then(resp => resp.json())
+    .then(Deck.appendDecks)
+  }
+
+  static appendDecks(decks){
+    const detailDiv = document.getElementById('detailContainer')
+    detailDiv.innerText = ''
+    const deckDiv = document.getElementById('indexContainer')
+    deckDiv.innerText = "All My Decks!"
+    for (let deck of decks){
+      const li = document.createElement('li')
+      li.innerText = `${deck.name} (${deck.group.name})`
+      deckDiv.append(li)
+    }
+  }
+
+  static newForm(){
+    const formCont = document.getElementById('newFormContainer')
+    formCont.innerHTML = ''
+    const form = document.createElement('form')
+    
+    const input = document.createElement('input')
+    input.setAttribute('type', 'text')
+    input.setAttribute('id', 'deckName')
+    input.setAttribute('placeholder', 'Name of New Deck')
+  
+    // const groupLabel = document.createElement('label')
+    // groupLabel.setAttribute('value', 'Group: ')
+    
+    // const group = document.createElement('select')
+    // const options = 
+    // for (let option of options)
+    // group.append
+    
+    const submit = document.createElement('input')
+    submit.setAttribute('type', 'submit')
+    submit.setAttribute('value', 'Submit')
+  
+    form.append(input, submit)
+    formCont.append(form)
+  
+    submit.addEventListener('click', Deck.createDeck)
+  }
+
+  static createDeck(e){
+    e.preventDefault()
+    const userInput = document.getElementById('deckName').value
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({deck: {name: userInput}})
+    }
+  
+    fetch("http://localhost:3000/groups", options)
+    .then(resp => resp.json())
+    .then(Deck.fetchDecks)
+  
+    e.target.parentElement.reset()
   }
 }
 
-function newDeckForm(){
-  const formCont = document.getElementById('newFormContainer')
-  formCont.innerHTML = ''
-  const form = document.createElement('form')
-  
-  const input = document.createElement('input')
-  input.setAttribute('type', 'text')
-  input.setAttribute('id', 'deckName')
-  input.setAttribute('placeholder', 'Name of New Deck')
 
-  // const groupLabel = document.createElement('label')
-  // groupLabel.setAttribute('value', 'Group: ')
-  
-  // const group = document.createElement('select')
-  // const options = 
-  // for (let option of options)
-  // group.append
-  
-  const submit = document.createElement('input')
-  submit.setAttribute('type', 'submit')
-  submit.setAttribute('value', 'Submit')
 
-  form.append(input, submit)
-  formCont.append(form)
 
-  submit.addEventListener('click', createDeck)
-}
 
-function createDeck(e){
-  e.preventDefault()
-  const userInput = document.getElementById('deckName').value
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({deck: {name: userInput}})
-  }
 
-  fetch("http://localhost:3000/groups", options)
-  .then(resp => resp.json())
-  .then(fetchDecks)
 
-  e.target.parentElement.reset()
-}
+
--- flashcard-frontend/src/index.js
+++ flashcard-frontend/src/index.js
@@ -2,12 +2,12 @@ const groupsButton = document.getElementById('allGroups')
 groupsButton.addEventListener('click', Group.fetchGroups)
 
 const decksButton = document.getElementById('allDecks') 
-decksButton.addEventListener('click', fetchDecks)
+decksButton.addEventListener('click', Deck.fetchDecks)
 
 const newGroupButton = document.getElementById('newGroup')
 newGroupButton.addEventListener('click', Group.newForm)
 
 const newDeckButton = document.getElementById('newDeck')
-newDeckButton.addEventListener('click', newDeckForm)
+newDeckButton.addEventListener('click', Deck.newForm)
 
 // fetchCards()
