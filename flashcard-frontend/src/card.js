acbcb6402da2ece2503fe457664be929582a6069
Cole Flournoy <flournoy.cole@gmail.com>
2021-05-09 22:49:13 -0400

    Refactor to Card class

--- flashcard-frontend/src/card.js
+++ flashcard-frontend/src/card.js
@@ -1,11 +1,25 @@
-function fetchCards(){
-  fetch("http://localhost:3000/cards")
-  .then(resp => resp.json())
-  .then(appendCards)
+class Card {
+  constructor(card){
+    this.id = card.id
+    this.front = card.front
+    this.back = card.back
+    this.deck = card.deck
+  }
+
+  static fetchCards(){
+    fetch("http://localhost:3000/cards")
+    .then(resp => resp.json())
+    .then(Card.appendCards)
+  }
+
+  static appendCards(cards){
+    for (let card of cards){
+      console.log(card)
+    }
+  }
+  
 }
 
-function appendCards(cards){
-  for (let card of cards){
-    console.log(card)
-  }
-}
+
+
+
